cdd5c247442fb54ae486f114d4761526ab6014dd
Ongthepmakemnhungnong <pazzzz123456@gmail.com>
2024-05-31 13:48:08 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,37 @@ export default function Home() {
       <main>
         <Header title="Welcome to my Sắt Thép Biên Hòa" />
         <p className="description">
-<h1><strong>Sắt Thép Biên Hòa</strong></h1>
-<div><a href="https://satthepbienhoa.vn/">Sắt Thép Biên Hòa</a> is one of the leading companies in the South, specializing in providing a variety of high quality Hoa Phat Steel Pipe and Hoa Phat steel square tube products. With many years of experience in the industry, we are committed to providing customers with prestige and outstanding quality.</div>
-<div> </div>
-<div>A strong point of Sat Thep Bien Hoa - Loc Thien Company is its competitive prices. We always strive to offer reasonable and market-appropriate prices, bringing customer satisfaction. At the same time, we not only focus on providing high-value products, but also always commit to dedicated and thoughtful service.</div>
-<div> </div>
-<div>With a team of experienced staff and deep understanding of the iron and steel industry, Sat Thep Bien Hoa - Loc Thien Company confidently brings customers the best quality products. We are not only a reliable supplier, but also a reliable partner in the construction and development of projects.</div>
-<div> </div>
-<div>With Sat Thep Bien Hoa, customers can be assured of the quality of iron and steel products and professional services. We commit to always be willing to support and accompany our customers to bring success and progress in every construction project.</div>
+<h2><span style="color: #000000;"><strong>Giới thiệu về tập đoàn Hòa Phát</strong></span></h2>
+<p><span style="color: #000000;">Thép Hòa Phát là một tập đoàn trong ngành công nghiệp thép tại Việt Nam được thành lập vào ngày 20/8/1996. Hòa Phát đã khẳng định vị thế của mình trong việc sản xuất và cung cấp các sản phẩm thép chất lượng cao như ống mạ kẽm nhúng nóng, ống thép đen hàn, ống thép tôn mạ kẽm, thép cuộn mạ kẽm nhúng nóng và ống thép hộp vuông, chữ nhật.</span></p>
+<p><span style="color: #000000;">Từ một nhà máy ban đầu tại Hưng Yên, Hòa Phát đã không ngừng phát triển và mở rộng quy mô sản xuất. Với việc xây dựng thêm 3 nhà máy sản xuất tại các tỉnh lân cận như Long An, Bình Dương và Đà Nẵng, Hòa Phát đã khẳng định vị thế của mình trong ngành công nghiệp thép Việt Nam.</span></p>
+<p><span style="color: #000000;">Hiện nay, Hòa Phát là một trong những tập đoàn sản xuất ống thép lớn nhất Việt Nam, với công suất sản xuất lớn ước tính khoảng 15 triệu tấn/năm.</span></p>
+<p><span style="color: #000000;">Công ty TNHH Ống thép Hòa Phát không ngừng phát triển và khẳng định vị thế của mình không chỉ qua quy mô sản xuất mà còn thông qua việc đạt được các tiêu chuẩn chất lượng cao, bao gồm các tiêu chuẩn quốc tế như:</span></p>
+<ul>
+<li><span style="color: #000000;"><strong>Ống thép hàn đen:</strong> TCVN3783 – 83 (VN), ASTM A500 (Mỹ), BS EN 10255:2004 (BS) 11387:1985 (Châu Âu).</span></li>
+<li><span style="color: #000000;"><strong>Ống thép hộp vuông, chữ nhật:</strong> TCVN3783 (VN), ASTM A500 (Mỹ).</span></li>
+<li><span style="color: #000000;"><strong>Ống thép tôn mạ kẽm:</strong> BS EN 10255:2004 (BS) 1387:1985 (Châu Âu).</span></li>
+<li><span style="color: #000000;"><span style="color: #000000;"><strong>Ống thép mạ kẽm nhúng nóng: </strong></span></span></li>
+<li><span style="color: #000000;"><strong>Thép cuộn mạ kẽm nhúng nóng:</strong> JIS G3302:2010 (Nhật Bản).</span></li>
+</ul>
+<figure>
+    <img src="https://satthepbienhoa.vn/wp-content/uploads/2024/04/Bang-gia-thep-hop-Hoa-Phat-tai-Binh-Duong.jpg" alt="thep hop hoa phat" style="width: 100%; padding: 5px; background-color: grey;">
+<h2><span style="color: #000000;"><strong>Ống thép mạ kẽm hòa phát là gì? </strong></span></h2>
+<p dir="auto" data-pm-slice="1 1 []"><span style="color: #000000;">Ống thép mạ kẽm Hòa Phát là một loại ống thép kẽm chất lượng cao và bề mặt kẽm bên ngoài sáng bóng, được sản xuất và cung cấp bởi Tập đoàn Hòa Phát một trong những doanh nghiệp hàng đầu trong lĩnh vực sản xuất và phân phối ống thép tại Việt Nam</span></p>
+<p dir="auto"><span style="color: #000000;">Ống thép mạ kẽm nhúng nóng Hòa Phát là sản phẩm chất lượng cao được ưa chuộng trên thị trường với đặc tính bền bỉ và khả năng chống ăn mòn tốt. Sản phẩm được thông qua quá trình mạ kẽm, sơn, xi hoặc tráng kẽm, giúp tăng cường độ bền và tuổi thọ.</span></p>
+<h3><span style="color: #000000;"><strong>Thông số kỹ thuật ống thép mạ kẽm hòa phát</strong></span></h3>
+<ul>
+<li><span style="color: #000000;"><strong>Loại sản phẩm: </strong>Ống thép mạ kẽm Hòa Phát</span></li>
+<li><span style="color: #000000;"><strong>Tiêu chuẩn:</strong> ASTM A53- Grade A (Mỹ) và BSEN 10255:2004 (Anh), BS 1387:1985 (Việt Nam).</span></li>
+<li><span style="color: #000000;"><strong>Kích thước:</strong> Ø21.2 – Ø 323.8.</span></li>
+<li><span style="color: #000000;"><strong>Mác thép:</strong> SS400, SAE, SPHC.</span></li>
+<li><span style="color: #000000;"><strong>Thương hiệu / Xuất xứ:</strong> Việt Nam.</span></li>
+<li><span style="color: #000000;"><strong>Độ dày: </strong>0.7mm – 6.37mm</span></li>
+<li><span style="color: #000000;"><strong>Trọng lượng ống thép mạ kẽm Hòa Phát: </strong>7,62kg - 199,886kg. (tùy vào kích thước ống thép)</span></li>
+<li><span style="color: #000000;"><strong>Chiều dài cây:</strong> 6m – 12m (Sản xuất theo yêu cầu của khách hàng với số lượng nhất định)</span></li>
+<li><span style="color: #000000;"><strong>Ứng dụng:</strong> Được sữ dụng trong các hệ thống PCCC, hệ thống cấp thoát nước, hệ thống xữ lý nước sạch,...</span></li>
+</ul>
+<h3><strong><span style="color: #000000;">Tiêu chuẩn sản xuất thép ống thép mạ kẽm Hòa Phát</span></strong></h3>
+<p><span style="color: #000000;">Tiêu chuẩn ống thép mạ kẽm hòa phát được sản xuất trên dây chuyền máy móc công nghệ cao và quy trình sản xuất khép kín, sản phẩm đem lại sự tin tưởng cho người tiêu dùng. Ống thép hòa phát không chỉ được đánh giá cao về chất lượng mà còn đáp ứng hoàn toàn các tiêu chuẩn nghiêm ngặt.</span>
 <div> </div>
 <div><strong>Contact Info:</strong></div>
 <div> </div>
