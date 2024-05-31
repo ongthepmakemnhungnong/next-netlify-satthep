6c9e110f86d85501838a5c9a06d38a829545eaa2
Ongthepmakemnhungnong <pazzzz123456@gmail.com>
2024-05-31 14:00:26 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,19 +18,16 @@ export default function Home() {
 <p><span style="color: #000000;">Từ một nhà máy ban đầu tại Hưng Yên, Hòa Phát đã không ngừng phát triển và mở rộng quy mô sản xuất. Với việc xây dựng thêm 3 nhà máy sản xuất tại các tỉnh lân cận như Long An, Bình Dương và Đà Nẵng, Hòa Phát đã khẳng định vị thế của mình trong ngành công nghiệp thép Việt Nam.</span></p>
 <p><span style="color: #000000;">Hiện nay, Hòa Phát là một trong những tập đoàn sản xuất ống thép lớn nhất Việt Nam, với công suất sản xuất lớn ước tính khoảng 15 triệu tấn/năm.</span></p>
 <p><span style="color: #000000;">Công ty TNHH Ống thép Hòa Phát không ngừng phát triển và khẳng định vị thế của mình không chỉ qua quy mô sản xuất mà còn thông qua việc đạt được các tiêu chuẩn chất lượng cao, bao gồm các tiêu chuẩn quốc tế như:</span></p>
-<ul>
 <li><span style="color: #000000;"><strong>Ống thép hàn đen:</strong> TCVN3783 – 83 (VN), ASTM A500 (Mỹ), BS EN 10255:2004 (BS) 11387:1985 (Châu Âu).</span></li>
 <li><span style="color: #000000;"><strong>Ống thép hộp vuông, chữ nhật:</strong> TCVN3783 (VN), ASTM A500 (Mỹ).</span></li>
 <li><span style="color: #000000;"><strong>Ống thép tôn mạ kẽm:</strong> BS EN 10255:2004 (BS) 1387:1985 (Châu Âu).</span></li>
 <li><span style="color: #000000;"><span style="color: #000000;"><strong>Ống thép mạ kẽm nhúng nóng: </strong></span></span></li>
 <li><span style="color: #000000;"><strong>Thép cuộn mạ kẽm nhúng nóng:</strong> JIS G3302:2010 (Nhật Bản).</span></li>
-</ul>
-    const element = <img src={https://satthepbienhoa.vn/wp-content/uploads/2024/04/Bang-gia-thep-hop-Hoa-Phat-tai-Binh-Duong.jpg}></img>
+    const element = <img src={https://satthepbienhoa.vn/wp-content/uploads/2024/04/Bang-gia-thep-hop-Hoa-Phat-tai-Binh-Duong.jpg}></img>;
 <h2><span style="color: #000000;"><strong>Ống thép mạ kẽm hòa phát là gì? </strong></span></h2>
 <p dir="auto" data-pm-slice="1 1 []"><span style="color: #000000;">Ống thép mạ kẽm Hòa Phát là một loại ống thép kẽm chất lượng cao và bề mặt kẽm bên ngoài sáng bóng, được sản xuất và cung cấp bởi Tập đoàn Hòa Phát một trong những doanh nghiệp hàng đầu trong lĩnh vực sản xuất và phân phối ống thép tại Việt Nam</span></p>
 <p dir="auto"><span style="color: #000000;">Ống thép mạ kẽm nhúng nóng Hòa Phát là sản phẩm chất lượng cao được ưa chuộng trên thị trường với đặc tính bền bỉ và khả năng chống ăn mòn tốt. Sản phẩm được thông qua quá trình mạ kẽm, sơn, xi hoặc tráng kẽm, giúp tăng cường độ bền và tuổi thọ.</span></p>
 <h3><span style="color: #000000;"><strong>Thông số kỹ thuật ống thép mạ kẽm hòa phát</strong></span></h3>
-<ul>
 <li><span style="color: #000000;"><strong>Loại sản phẩm: </strong>Ống thép mạ kẽm Hòa Phát</span></li>
 <li><span style="color: #000000;"><strong>Tiêu chuẩn:</strong> ASTM A53- Grade A (Mỹ) và BSEN 10255:2004 (Anh), BS 1387:1985 (Việt Nam).</span></li>
 <li><span style="color: #000000;"><strong>Kích thước:</strong> Ø21.2 – Ø 323.8.</span></li>
@@ -40,7 +37,6 @@ export default function Home() {
 <li><span style="color: #000000;"><strong>Trọng lượng ống thép mạ kẽm Hòa Phát: </strong>7,62kg - 199,886kg. (tùy vào kích thước ống thép)</span></li>
 <li><span style="color: #000000;"><strong>Chiều dài cây:</strong> 6m – 12m (Sản xuất theo yêu cầu của khách hàng với số lượng nhất định)</span></li>
 <li><span style="color: #000000;"><strong>Ứng dụng:</strong> Được sữ dụng trong các hệ thống PCCC, hệ thống cấp thoát nước, hệ thống xữ lý nước sạch,...</span></li>
-</ul>
 <h3><strong><span style="color: #000000;">Tiêu chuẩn sản xuất thép ống thép mạ kẽm Hòa Phát</span></strong></h3>
 <p><span style="color: #000000;">Tiêu chuẩn ống thép mạ kẽm hòa phát được sản xuất trên dây chuyền máy móc công nghệ cao và quy trình sản xuất khép kín, sản phẩm đem lại sự tin tưởng cho người tiêu dùng. Ống thép hòa phát không chỉ được đánh giá cao về chất lượng mà còn đáp ứng hoàn toàn các tiêu chuẩn nghiêm ngặt.</span>
 <div> </div>
