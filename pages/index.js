43cfe978ca75d57f88789e572607fc91fe7395bc
Ongthepmakemnhungnong <pazzzz123456@gmail.com>
2024-05-31 13:34:41 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
- 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hiển thị danh sách bài viết bằng html css</title>
-    <link rel="stylesheet" href="style.css">
-</head>
- 
-<body>
- 
-    <div id="wapper">
-        <h1>Tạo danh sách bài viết bằng html css</h1>
-        <ul class="list-post">
-            <li>
-                <a href="" class="post-thumb">
-                    <img src="images/pic-1.jpg" alt="">
-                </a>
-                <div class="more-info">
-                    <a href="" class="post-title">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, in.
-                    </a>
-                    <p class="post-excerpt">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque est facilis fuga excepturi aut cupiditate repellendus dolor itaque nihil neque?
-                    </p>
-                </div>
-            </li>
-            <li>
-                <a href="" class="post-thumb">
-                    <img src="images/pic-2.jpg" alt="">
-                </a>
-                <div class="more-info">
-                    <a href="" class="post-title">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, in.
-                    </a>
-                    <p class="post-excerpt">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque est facilis fuga excepturi aut cupiditate repellendus dolor itaque nihil neque?
-                    </p>
-                </div>
-            </li>
-        </ul>
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my Sắt Thép Biên Hòa" />
+        <p className="description">
+<h1><strong>Sắt Thép Biên Hòa</strong></h1>
+<div><a href="https://satthepbienhoa.vn/">Sắt Thép Biên Hòa</a> is one of the leading companies in the South, specializing in providing a variety of high quality Hoa Phat Steel Pipe and Hoa Phat steel square tube products. With many years of experience in the industry, we are committed to providing customers with prestige and outstanding quality.</div>
+<div> </div>
+<div>A strong point of Sat Thep Bien Hoa - Loc Thien Company is its competitive prices. We always strive to offer reasonable and market-appropriate prices, bringing customer satisfaction. At the same time, we not only focus on providing high-value products, but also always commit to dedicated and thoughtful service.</div>
+<div> </div>
+<div>With a team of experienced staff and deep understanding of the iron and steel industry, Sat Thep Bien Hoa - Loc Thien Company confidently brings customers the best quality products. We are not only a reliable supplier, but also a reliable partner in the construction and development of projects.</div>
+<div> </div>
+<div>With Sat Thep Bien Hoa, customers can be assured of the quality of iron and steel products and professional services. We commit to always be willing to support and accompany our customers to bring success and progress in every construction project.</div>
+<div> </div>
+<div><strong>Contact Info:</strong></div>
+<div> </div>
+<div>Website: <a href="https://satthepbienhoa.vn/">satthepbienhoa.vn</a></div>
+<div> </div>
+<div>
+<div><a href="https://www.google.com/maps/place/S%E1%BA%AFt+Th%C3%A9p+Bi%C3%AAn+H%C3%B2a/@10.9485282,106.8694035,17z/data=!3m1!4b1!4m6!3m5!1s0x3174dd0400a16e81:0xa23dafa3fcde8ea5!8m2!3d10.9485282!4d106.8694035!16s%2Fg%2F11nmh1c792?hl=vi-VN&amp;entry=ttu">Google Maps</a></div>
+<div> </div>
+<div><a href="https://www.facebook.com/congtylocthien">Facebook fanpage</a></div>
+<div> </div>
+<div>Visit the <a href="https://sites.google.com/view/satthepbienhoa/trang-ch%E1%BB%A7">Google Sites website</a></div>
+</div>
+        </p>
+      </main>
+
+      <Footer />
     </div>
- 
-</body>
- 
-</html>
+  )
+}
